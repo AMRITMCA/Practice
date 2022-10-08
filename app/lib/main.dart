32a0042259b46f0221d31689589eabd1aa8645b1
AMRITMCA <amritkumar0201999@gmail.com>
2022-10-08 20:16:18 +0530

    homepage

---
+++ app/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my app',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Center(
+        child: Text(
+          'Amrit',
+          style: TextStyle(
+            color: Colors.amber,
+            fontSize: 55,
+          ),
+        ),
+      ),
+    );
+  }
+}
